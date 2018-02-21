2ecf9842b40ddf113099001ec57e84d7cd128716
Hao Hou <ghost89413@gmail.com>
2018-02-20 17:39:48 -0700

    Source code base line

---
+++ script/shell.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/zsh
+
+export PATH=${PATH}:${STARTUP_DIR}/../script/
+cd ${STARTUP_DIR} && zsh
+
---
+++ src/setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/zsh
+
+export STARTUP_DIR=`pwd`
+
+cd ../plumber_examples/ 
+
+./init --shell=../script/shell.sh
